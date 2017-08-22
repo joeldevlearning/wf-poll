74ae1244ca408d9f60cce071cb3ea9f6c06570a3
webdevjoel <webdevjoel@outlook.com>
2017-08-22 17:01:32 -0400

    larger font sizes

--- wf-poll-cwe/PollForm.Designer.cs
+++ wf-poll-cwe/PollForm.Designer.cs
@@ -41,10 +41,11 @@
             // CandidateOne
             //
             this.CandidateOne.AutoSize = true;
-            this.CandidateOne.Location = new System.Drawing.Point(41, 52);
-            this.CandidateOne.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CandidateOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CandidateOne.Location = new System.Drawing.Point(41, 82);
+            this.CandidateOne.Margin = new System.Windows.Forms.Padding(4);
             this.CandidateOne.Name = "CandidateOne";
-            this.CandidateOne.Size = new System.Drawing.Size(84, 21);
+            this.CandidateOne.Size = new System.Drawing.Size(127, 33);
             this.CandidateOne.TabIndex = 0;
             this.CandidateOne.TabStop = true;
             this.CandidateOne.Text = poller.GetCandidates().CandidateOne;
@@ -54,10 +55,11 @@
             // CandidateTwo
             //
             this.CandidateTwo.AutoSize = true;
-            this.CandidateTwo.Location = new System.Drawing.Point(41, 80);
-            this.CandidateTwo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CandidateTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CandidateTwo.Location = new System.Drawing.Point(41, 130);
+            this.CandidateTwo.Margin = new System.Windows.Forms.Padding(4);
             this.CandidateTwo.Name = "CandidateTwo";
-            this.CandidateTwo.Size = new System.Drawing.Size(132, 21);
+            this.CandidateTwo.Size = new System.Drawing.Size(210, 33);
             this.CandidateTwo.TabIndex = 1;
             this.CandidateTwo.TabStop = true;
             this.CandidateTwo.Text = poller.GetCandidates().CandidateTwo;
@@ -70,11 +72,12 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.CandidateTwo);
             this.groupBox1.Controls.Add(this.CandidateOne);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(16, 15);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(399, 201);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(501, 217);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Vote";
@@ -82,8 +85,9 @@
             //
             // butSubmit
             //
-            this.butSubmit.Location = new System.Drawing.Point(132, 120);
-            this.butSubmit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.butSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butSubmit.Location = new System.Drawing.Point(334, 94);
+            this.butSubmit.Margin = new System.Windows.Forms.Padding(4);
             this.butSubmit.Name = "butSubmit";
             this.butSubmit.Size = new System.Drawing.Size(120, 60);
             this.butSubmit.TabIndex = 3;
@@ -94,27 +98,30 @@
             // label1
             //
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(23, 27);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(210, 17);
+            this.label1.Size = new System.Drawing.Size(290, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Who is your favorite Candidate?";
             //
             // lblResults
             //
-            this.lblResults.Location = new System.Drawing.Point(83, 266);
+            this.lblResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResults.Location = new System.Drawing.Point(44, 292);
             this.lblResults.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblResults.Name = "lblResults";
-            this.lblResults.Size = new System.Drawing.Size(267, 98);
+            this.lblResults.Size = new System.Drawing.Size(426, 162);
             this.lblResults.TabIndex = 3;
             //
             // butResults
             //
-            this.butResults.Location = new System.Drawing.Point(148, 224);
-            this.butResults.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.butResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butResults.Location = new System.Drawing.Point(130, 240);
+            this.butResults.Margin = new System.Windows.Forms.Padding(4);
             this.butResults.Name = "butResults";
-            this.butResults.Size = new System.Drawing.Size(120, 28);
+            this.butResults.Size = new System.Drawing.Size(235, 48);
             this.butResults.TabIndex = 6;
             this.butResults.Text = "Show Results";
             this.butResults.UseVisualStyleBackColor = true;
@@ -124,11 +131,11 @@
             //
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(428, 435);
+            this.ClientSize = new System.Drawing.Size(530, 515);
             this.Controls.Add(this.butResults);
             this.Controls.Add(this.lblResults);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "PollForm";
             this.Text = "Polling System";
             this.Load += new System.EventHandler(this.PollForm_Load);
