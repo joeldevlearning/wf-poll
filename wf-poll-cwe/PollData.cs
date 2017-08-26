bc28f8a63ec99dc6f0395dd23f73d98d4ca6bd91
webdevjoel <webdevjoel@outlook.com>
2017-08-25 21:45:08 -0400

    added PollData class

---
+++ wf-poll-cwe/PollData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wf_poll
+{
+    class PollData
+    {
+        public IReadOnlyList<Candidate> Candidates;
+    }
+
+    class Candidate
+    {
+        public readonly string Name;
+        public readonly Guid Id;
+
+        public Candidate(string name)
+        {
+            name = Name;
+            Id = new Guid();
+
+        }
+    }
+}
